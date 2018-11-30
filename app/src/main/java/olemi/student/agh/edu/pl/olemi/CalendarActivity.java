4a28ac30e149b6b3cdc3fbb88f22fe37a8e79df2
Arkadiusz Olesek <arkadiusz.olesek@gmail.com>
2018-11-30 19:21:06 +0100

    Day, AddMeal and Calendar Activites added

---
+++ app/src/main/java/olemi/student/agh/edu/pl/olemi/CalendarActivity.java
@@ -0,0 +1,13 @@
+package olemi.student.agh.edu.pl.olemi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
