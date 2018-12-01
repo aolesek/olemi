13f498114f92633bfb06f26a449926716906b7bb
maciek96 <mmika1496@gmail.com>
2018-12-01 21:59:50 +0100

    simple search

--- app/src/main/java/pl/edu/agh/student/olemi/AddMealActivity.java
+++ app/src/main/java/pl/edu/agh/student/olemi/AddMealActivity.java
@@ -1,13 +1,91 @@
 package pl.edu.agh.student.olemi;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
 
-public class AddMealActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import pl.edu.agh.student.olemi.helpers.MealAdapter;
+import pl.edu.agh.student.olemi.model.Nutrients;
+import pl.edu.agh.student.olemi.model.ProductModel;
+import pl.edu.agh.student.olemi.model.SimpleProduct;
+
+public class AddMealActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
+
+    private SearchView searchView;
+    private MenuItem searchMenuItem;
+    private MealAdapter mealAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_meal);
+
+
+        initList();
+        setActionBar();
+    }
+
+    public void initList(){
+        List<ProductModel> productModels = new ArrayList<>();
+        productModels.add(new SimpleProduct("jajko", new Nutrients(2.137, 2.137, 2.137, 2.137)));
+        productModels.add(new SimpleProduct("bozena", new Nutrients(2.1, 2.137, 2.137, 2.137)));
+        productModels.add(new SimpleProduct("abc", new Nutrients(1.1, 2.137, 2.137, 2.137)));
+        mealAdapter = new MealAdapter(this, productModels);
+        ListView listView = findViewById(R.id.list_new_meal_search);
+        listView.setAdapter(mealAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+
+        SearchManager searchManager = (SearchManager)
+                getSystemService(Context.SEARCH_SERVICE);
+        searchMenuItem = menu.findItem(R.id.search);
+        searchView = (SearchView) searchMenuItem.getActionView();
+
+        searchView.setSearchableInfo(searchManager.
+                getSearchableInfo(getComponentName()));
+        searchView.setSubmitButtonEnabled(true);
+        searchView.setOnQueryTextListener(this);
+
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        mealAdapter.getFilter().filter(newText);
+        return true;
+    }
+
+    private void setActionBar() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Friends");
+
+//        Typeface typeface = Typeface..createFromAsset(this.getAssets(), "fonts/vegur_2.otf");
+//        int titleId = getResources().getIdentifier("action_bar_title", "id", "android");
+//        TextView actionBarTitle = (TextView) (this.findViewById(titleId));
+//       // actionBarTitle.setTextColor(getResources().getColor(R.color.white));
+//        actionBarTitle.setTypeface(typeface);
     }
 }
+
--- app/src/main/java/pl/edu/agh/student/olemi/helpers/MealAdapter.java
+++ app/src/main/java/pl/edu/agh/student/olemi/helpers/MealAdapter.java
@@ -1,13 +1,14 @@
 package pl.edu.agh.student.olemi.helpers;
 
 import android.content.Context;
-import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -16,16 +17,54 @@ import java.util.List;
 import pl.edu.agh.student.olemi.R;
 import pl.edu.agh.student.olemi.model.ProductModel;
 
-public class MealAdapter extends ArrayAdapter<ProductModel> {
+public class MealAdapter extends ArrayAdapter<ProductModel> implements Filterable {
 
     private Context mContext;
-    private List<ProductModel> mealList = new ArrayList<>();
+    private List<ProductModel> mealList;
+    private MealFilter mealFilter;
+    private List<ProductModel> filteredList;
 
     public MealAdapter(@NonNull Context context, List<ProductModel> list) {
         super(context, 0, list);
-        mContext = context;
-        mealList = list;
+        this.mContext = context;
+        this.mealList = list;
+        this.filteredList = list;
+
+        getFilter();
     }
+
+    /**
+     * Get size of user list
+     * @return userList size
+     */
+    @Override
+    public int getCount() {
+        if(filteredList != null)
+            return filteredList.size();
+        else
+            return 0;
+    }
+
+    /**
+     * Get specific item from user list
+     * @param i item index
+     * @return list item
+     */
+    @Override
+    public ProductModel getItem(int i) {
+        return filteredList.get(i);
+    }
+
+    /**
+     * Get user list item id
+     * @param i item index
+     * @return current item id
+     */
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
 
     @NonNull
     @Override
@@ -51,5 +90,55 @@ public class MealAdapter extends ArrayAdapter<ProductModel> {
         TextView list_meal_info_calories = (TextView) listItem.findViewById(R.id.list_meal_info_calories);
         list_meal_info_calories.setText("Cal: "+productModel.getNutrients().getCalories().toString());
         return listItem;
+    }
+
+    @Override
+    public Filter getFilter() {
+        if (mealFilter == null) {
+            mealFilter = new MealFilter();
+        }
+        return mealFilter;
+    }
+
+    private class MealFilter extends Filter {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults filterResults = new FilterResults();
+            if (constraint!=null && constraint.length()>0) {
+                ArrayList<ProductModel> tempList = new ArrayList<ProductModel>();
+
+                // search content in friend list
+                for (ProductModel productModel : mealList) {
+                    if (productModel.getName().toLowerCase().contains(constraint.toString().toLowerCase())) {
+                        tempList.add(productModel);
+                        System.out.println(productModel.getName());
+                    }
+                }
+
+                filterResults.count = tempList.size();
+                filterResults.values = tempList;
+            } else {
+                filterResults.count = mealList.size();
+                filterResults.values = mealList;
+            }
+
+            System.out.println(((List<ProductModel>) filterResults.values).get(0).getName());
+            return filterResults;
+        }
+
+        /**
+         * Notify about filtered list to ui
+         * @param constraint text
+         * @param results filtered result
+         */
+        @SuppressWarnings("unchecked")
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            filteredList = (List<ProductModel>) results.values;
+            clear();
+            addAll(filteredList);
+            notifyDataSetChanged();
+        }
     }
 }
