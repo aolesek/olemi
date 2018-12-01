c31bc4cb2b48709fc5c4267ece6b4fc9ac04d7c3
maciek96 <mmika1496@gmail.com>
2018-12-01 19:11:07 +0100

    alpha version of dayActivity

---
+++ app/src/main/java/pl/edu/agh/student/olemi/model/ProductModel.java
@@ -0,0 +1,28 @@
+package pl.edu.agh.student.olemi.model;
+
+public abstract class ProductModel {
+
+    String name;
+    Nutrients nutrients;
+
+    public ProductModel(String name, Nutrients nutrients) {
+        this.name = name;
+        this.nutrients = nutrients;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Nutrients getNutrients() {
+        return nutrients;
+    }
+
+    public void setNutrients(Nutrients nutrients) {
+        this.nutrients = nutrients;
+    }
+}
